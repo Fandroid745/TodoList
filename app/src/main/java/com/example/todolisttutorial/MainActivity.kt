e90b6756bd24e1fe40d25953ec6661cf30e524ce
Fandroid745 <dhanush41230@gmail.com>
2024-03-06 22:29:05 +0530

    added enhancements to  the to do list app

--- app/src/main/java/com/example/todolisttutorial/MainActivity.kt
+++ app/src/main/java/com/example/todolisttutorial/MainActivity.kt
@@ -1,30 +1,60 @@
+
 package com.example.todolisttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todolisttutorial.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), TaskItemClickListener
 {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var taskViewModel: TaskViewModel
+    //private lateinit var taskViewModel: TaskViewModel
+    private val taskViewModel: TaskViewModel by viewModels {
+        TaskItemModelFactory((application as TodoApplication).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
-        binding.newTaskButton.setOnClickListener {
-            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
-        }
+        //taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
 
-        taskViewModel.name.observe(this){
-            binding.taskName.text = String.format("Task Name: %s", it)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
-        taskViewModel.desc.observe(this){
-            binding.taskDesc.text = String.format("Task Desc: %s", it)
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView()
+    {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
         }
     }
+
+    override fun editTaskItem(taskItem: TaskItem)
+    {
+        NewTaskSheet(taskItem).show(supportFragmentManager,"newTaskTag")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem)
+    {
+        taskViewModel.setCompleted(taskItem)
+    }
 }
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/todolisttutorial/TaskViewModel.kt
+++ app/src/main/java/com/example/todolisttutorial/TaskViewModel.kt
@@ -1,10 +1,36 @@
+
 package com.example.todolisttutorial
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+import java.time.LocalDate
 
-class TaskViewModel: ViewModel()
+class TaskViewModel(private val repository: TaskItemRepository): ViewModel()
 {
-    var name = MutableLiveData<String>()
-    var desc = MutableLiveData<String>()
+    val taskItems: LiveData<List<TaskItem>> = repository.allTaskItems.asLiveData()
+
+    fun addTaskItem(taskItem: TaskItem) = viewModelScope.launch {
+        repository.insertTaskItem(taskItem)
+    }
+
+    fun updateTaskItem(taskItem: TaskItem) = viewModelScope.launch {
+        repository.updateTaskItem(taskItem)
+    }
+
+    fun setCompleted(taskItem: TaskItem) = viewModelScope.launch {
+        if (!taskItem.isCompleted())
+            taskItem.completedDateString = TaskItem.dateFormatter.format(LocalDate.now())
+        repository.updateTaskItem(taskItem)
+    }
+}
+
+class TaskItemModelFactory(private val repository: TaskItemRepository) : ViewModelProvider.Factory
+{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T
+    {
+        if (modelClass.isAssignableFrom(TaskViewModel::class.java))
+            return TaskViewModel(repository) as T
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
